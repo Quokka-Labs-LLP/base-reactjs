ba2228d0ea17d1135d6dc5ed129fc2b5653ac306
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2024-10-19 20:33:20 +0530

    [feat] (arch): Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,18 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
+import sonarjs from 'eslint-plugin-sonarjs'
+
+// const OFF = 0
+// const WARN = 1
+const ERROR = 2
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {ignores: ['dist', 'build', 'coverage', 'node_modules']},
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, sonarjs.configs.recommended],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -16,13 +22,13 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      unicorn: eslintPluginUnicorn,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
+      'no-console': ERROR,
+      '@typescript-eslint/explicit-function-return-type': ERROR
     },
-  },
+  }
 )
