402a627cb73a8385ffc2489d4e02ac2cbae06931
QL-DEEPAK-RAWAT <deepak.rawat@quokkalabs.com>
2024-10-21 15:01:56 +0530

    (feature) added export csv and uplaod csv

---
+++ src/lib/exportCsv.ts
@@ -0,0 +1,18 @@
+import * as XLSX from 'xlsx'
+const downloadCsv = (data: any[], fileName: string) => {
+  const worksheet = XLSX.utils.json_to_sheet(data)
+  const csv = XLSX.utils.sheet_to_csv(worksheet)
+  const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
+  const link = document.createElement('a')
+  if (link.download !== undefined) {
+    const url = URL.createObjectURL(blob)
+    link.setAttribute('href', url)
+    link.setAttribute('download', `${fileName}.csv`)
+    link.style.visibility = 'hidden'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+}
+
+export default downloadCsv
